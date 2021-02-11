622b4c48609e4209e7196673630d7e9ef28be7c0
metalex9 <alexbainter@gmail.com>
2021-02-11 14:08:33 -0600

    Add legacy data export

--- scripts/render-page.js
+++ scripts/render-page.js
@@ -3,19 +3,19 @@
 const fsp = require('fs').promises;
 const path = require('path');
 const rollup = require('rollup');
-const csso = require('csso');
 const { minify } = require('html-minifier');
 const { createElement } = require('react');
 const { renderToStaticMarkup } = require('react-dom/server');
 const createRollupConfig = require('./create-rollup-config');
 
 const renderPageComponent = (component) =>
-  fsp
-    .readFile(path.join(__dirname, '../src/styles.css'), 'utf8')
-    .then((styles) => {
-      const { css } = csso.minify(styles);
-      const staticMarkup = renderToStaticMarkup(createElement(component));
-      const html = `<!DOCTYPE html>
+  Promise.all(
+    ['../src/styles.css', '../src/page-script.js'].map((filepath) =>
+      fsp.readFile(path.join(__dirname, filepath), 'utf8')
+    )
+  ).then(([css, script]) => {
+    const staticMarkup = renderToStaticMarkup(createElement(component));
+    const html = `<!DOCTYPE html>
     <html lang="en">
       <head>
         <meta charset="utf-8" />
@@ -23,14 +23,19 @@ const renderPageComponent = (component) =>
           name="viewport"
           content="width=device-width, initial-scale=1, minimum-scale=1"
         />
-        <style>${css}</style>
         <link rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+        <style>${css}</style>
+        <script type="text/javascript">${script}</script>
       </head>
       <body>${staticMarkup}</body>
     </html>`;
-      return minify(html, { collapseWhitespace: true });
+    return minify(html, {
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true,
     });
+  });
 
 const renderPage = (modulePath) => {
   const rollupConfig = createRollupConfig(modulePath);
---
+++ src/page-script.js
@@ -0,0 +1,60 @@
+(function () {
+  var openerOrigin;
+  switch (location.origin) {
+    case 'https://generative.fm': {
+      openerOrigin = 'https://play.generative.fm';
+      break;
+    }
+    case 'https://staging.generative.fm': {
+      openerOrigin = 'https://staging.play.generative.fm';
+      break;
+    }
+    case 'http://localhost:3000': {
+      openerOrigin = 'http://localhost:8080';
+      break;
+    }
+    default: {
+      // do nothing
+    }
+  }
+  if (!openerOrigin) {
+    return;
+  }
+  window.addEventListener('message', function (event) {
+    var data = event.data;
+    var source = event.source;
+    var origin = event.origin;
+    if (origin !== openerOrigin) {
+      return;
+    }
+    if (data.type === 'export-request') {
+      var state = window.localStorage.getItem('STATE');
+      source.postMessage({ type: 'export', state: state }, origin);
+      return;
+    }
+    if (data.type === 'set-import-request') {
+      var stateString = window.localStorage.getItem('STATE');
+      if (typeof stateString !== 'string') {
+        window.localStorage.setItem('STATE', '{isImported:true}');
+        return;
+      }
+      try {
+        state = JSON.parse(stateString);
+      } catch (error) {
+        console.error('Unable to parse stored state', error);
+        return;
+      }
+      if (state === null || typeof state !== 'object') {
+        window.localStorage.setItem('STATE', '{isImported:true}');
+        return;
+      }
+      state.isImported = true;
+      try {
+        window.localStorage.setItem('STATE', JSON.stringify(state));
+      } catch (err) {
+        console.error('Unable to set state as imported', err);
+      }
+      source.postMessage({ type: 'import-set' }, origin);
+    }
+  });
+})();
